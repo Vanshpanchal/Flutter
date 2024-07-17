fe7063718ecdf8a015cb28b2bbb462d18eabf157
Vansh-Panchal <vansh.panchal719@gmail.com>
2024-07-17 17:44:51 +0530

    Login Signup Finish

--- lib/login.dart
+++ lib/login.dart
@@ -11,10 +11,14 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  final TextEditingController email_controller = TextEditingController();
+  final TextEditingController password_controller = TextEditingController();
+  bool _isPasswordVisible = false;
+
   signIn() async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: "vansh.tapu@gmail.com", password: "1234567890");
+          email: email_controller.text, password: password_controller.text);
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text(e.code)));
@@ -22,9 +26,11 @@ class _loginState extends State<login> {
       debugPrint("Signupcode  {$e}");
     }
   }
+
   forget() async {
     try {
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: "vansh.tapu@gmail.com");
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: "vansh.tapu@gmail.com");
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text(e.code)));
@@ -32,32 +38,110 @@ class _loginState extends State<login> {
       debugPrint("Signupcode  {$e}");
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
+      backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                signIn();
-              },
-              child: Text("Login"),
-            ),
-            ElevatedButton(
-              onPressed: (() => {Get.to(signup())}),
-              child: Text("SignUp"),
-            ),
-            ElevatedButton(
-              child: Text("Forget"),
-              onPressed: () {
-                forget();
-              },
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                'Welcome Back',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: email_controller,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  filled: true,
+                  fillColor: Colors.grey[800],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: password_controller,
+                obscureText: !_isPasswordVisible,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: Colors.grey[800],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),color: Colors.white60,
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    )),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  signIn();
+                  // Handle login logic here
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  backgroundColor: Colors.blueAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  forget();
+                  // Handle forgot password logic here
+                },
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              TextButton(
+                onPressed: (() => {Get.to(signup())}),
+                child: const Text(
+                  'Don\'t have an account? Sign Up',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,3 +1,4 @@
+import 'package:apk/login.dart';
 import 'package:apk/wrapper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -11,11 +12,13 @@ class signup extends StatefulWidget {
 }
 
 class _signupState extends State<signup> {
+  final TextEditingController email_controller = TextEditingController();
+  final TextEditingController password_controller = TextEditingController();
+  bool _isPasswordVisible = false;
   signup() async {
-
     try {
-      await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: "vansh.tapu@gmail.com", password: "123456789");
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email_controller.text, password: password_controller.text);
       Get.offAll(wrapper());
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context)
@@ -25,27 +28,124 @@ class _signupState extends State<signup> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sign Up"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              child: Text("SignUp"),
-              onPressed: () {
-                signup();
-              },
+        backgroundColor: Colors.black,
+        body: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
             ),
-
-          ],
-        ),
-      ),
-    );
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      'Create Account',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.grey[800],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: email_controller,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.grey[800],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: password_controller,
+                      obscureText: !_isPasswordVisible,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.grey[800],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),color: Colors.white60,
+                            onPressed: () {
+                              setState(() {
+                                _isPasswordVisible = !_isPasswordVisible;
+                              });
+                            },
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        signup();
+                        // Handle signup logic here
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        backgroundColor: Colors.blueAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(fontSize: 18,color: Colors.white),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: (() => {Get.to(login())}),
+                      child: Text(
+                        'Already have an account? Login',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
