e5b76f6d7e15fa5d95f4f147fbbdee4d1b59ba77
Vansh-Panchal <vansh.panchal719@gmail.com>
2024-07-17 21:16:00 +0530

    Created user db

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,7 +1,12 @@
+import 'dart:ui';
+
 import 'package:apk/login.dart';
 import 'package:apk/wrapper.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:get/get.dart';
 
 class signup extends StatefulWidget {
@@ -14,11 +19,40 @@ class signup extends StatefulWidget {
 class _signupState extends State<signup> {
   final TextEditingController email_controller = TextEditingController();
   final TextEditingController password_controller = TextEditingController();
+  final TextEditingController username_controller = TextEditingController();
   bool _isPasswordVisible = false;
+
   signup() async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: email_controller.text, password: password_controller.text);
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: email_controller.text, password: password_controller.text);
+      Map<String, dynamic> userData = {
+        'Username': username_controller.text,
+        'Email': userCredential.user?.email,
+        'Uid': userCredential.user?.uid,
+        'Amdin': false
+      };
+      await FirebaseFirestore.instance
+          .collection("User")
+          .doc(userCredential.user?.uid)
+          .set(userData)
+          .then((_) => {
+                debugPrint("AddUser: User Added"),
+                Get.showSnackbar(const GetSnackBar(
+                  title: "User-Creation",
+                  message: "Success",
+                  icon: Icon(
+                    Icons.cloud_done_sharp,
+                    color: Colors.white,
+                  ),
+                  duration: Duration(seconds: 3),
+                ))
+              })
+          .catchError((e) {
+        debugPrint("AddUser  {$e}");
+      });
+
       Get.offAll(wrapper());
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context)
@@ -55,6 +89,7 @@ class _signupState extends State<signup> {
                     ),
                     SizedBox(height: 20),
                     TextField(
+                      controller: username_controller,
                       style: TextStyle(color: Colors.white),
                       decoration: InputDecoration(
                         hintText: 'Username',
@@ -92,34 +127,36 @@ class _signupState extends State<signup> {
                       obscureText: !_isPasswordVisible,
                       style: TextStyle(color: Colors.white),
                       decoration: InputDecoration(
-                        hintText: 'Password',
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Colors.grey[800],
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                          hintText: 'Password',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          filled: true,
+                          fillColor: Colors.grey[800],
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 20),
                           suffixIcon: IconButton(
                             icon: Icon(
                               _isPasswordVisible
                                   ? Icons.visibility
                                   : Icons.visibility_off,
-                            ),color: Colors.white60,
+                            ),
+                            color: Colors.white60,
                             onPressed: () {
                               setState(() {
                                 _isPasswordVisible = !_isPasswordVisible;
                               });
                             },
-                          )
-                      ),
+                          )),
                     ),
                     SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
                         signup();
+
+                        // adduser(username_controller.text, email_controller.text);
                         // Handle signup logic here
                       },
                       style: ElevatedButton.styleFrom(
@@ -131,7 +168,7 @@ class _signupState extends State<signup> {
                       ),
                       child: Text(
                         'Sign Up',
-                        style: TextStyle(fontSize: 18,color: Colors.white),
+                        style: TextStyle(fontSize: 18, color: Colors.white),
                       ),
                     ),
                     TextButton(
